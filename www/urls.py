6b1b05a94a818aa616b070b83b07c62c43393455
Foad Nosrati Habibi <foad.nh@gmail.com>
2013-03-15 00:21:53 +0330

    Fix urls.py problems

--- www/urls.py
+++ www/urls.py
@@ -5,19 +5,19 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'www.views.home', name='home'),
-    # url(r'^www/', include('www.foo.urls')),
+	# Examples:
+	# url(r'^$', 'www.views.home', name='home'),
+	# url(r'^www/', include('www.foo.urls')),
 	url(r'^$', 'root.views.home'),
 	url(r'^about/', 'root.views.about'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the admin/doc line below to enable admin documentation:
+	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+	# Uncomment the next line to enable the admin:
+	url(r'^admin/', include(admin.site.urls)),
 
 	# Zinnia
-	url(r'^weblog/', include('zinnia.urls')),
+	url(r'^blog/', include('zinnia.urls')),
 	url(r'^comments/', include('django.contrib.comments.urls')),
 )
